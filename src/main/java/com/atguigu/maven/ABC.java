139c9defb9eefb97dadc30ec5e5c6927eb0d6fa5
林凯 <278741657@qq.com>
2021-02-02 22:32:55 +0800

    修改了第5行代码，增加了master

--- src/main/java/com/atguigu/maven/ABC.java
+++ src/main/java/com/atguigu/maven/ABC.java
@@ -2,6 +2,6 @@ package com.atguigu.maven;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("11111111");
+        System.out.println("11111111master");
     }
 }
