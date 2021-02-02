7217a0473c6c2462664b1c2c8726d55022a829a4
林凯 <78408648+linkai521@users.noreply.github.com>
2021-02-02 23:05:07 +0800

    Update ABC.java

--- src/main/java/com/atguigu/maven/ABC.java
+++ src/main/java/com/atguigu/maven/ABC.java
@@ -3,10 +3,10 @@ package com.atguigu.maven;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("11111111master");
-        System.out.println("11111111DEV");
-        System.out.println("11111111DEV");
-        System.out.println("11111111DEV");
-        System.out.println("11111111DEV");
-        System.out.println("11111111DEV");
+        System.out.println("11111111DEV11");
+        System.out.println("11111111DEV11");
+        System.out.println("11111111DEV11");
+        System.out.println("11111111DEV11");
+        System.out.println("11111111DEV11");
     }
 }
