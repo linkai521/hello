5bae7f3622b7130c7cc1a4676af35a9aa82eb3e1
林凯 <278741657@qq.com>
2021-02-02 23:03:04 +0800

    增加了多行DEV

--- src/main/java/com/atguigu/maven/ABC.java
+++ src/main/java/com/atguigu/maven/ABC.java
@@ -4,5 +4,9 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("11111111master");
         System.out.println("11111111DEV");
+        System.out.println("11111111DEV");
+        System.out.println("11111111DEV");
+        System.out.println("11111111DEV");
+        System.out.println("11111111DEV");
     }
 }
