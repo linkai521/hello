0c44b50250f79219379a0cde80e5bc982967256a
林凯 <278741657@qq.com>
2021-02-02 23:32:24 +0800

    修改了第5行

--- src/main/java/com/atguigu/maven/ABC.java
+++ src/main/java/com/atguigu/maven/ABC.java
@@ -2,7 +2,7 @@ package com.atguigu.maven;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("11111111master");
+        System.out.println("11111111dev");
 
     }
 }
